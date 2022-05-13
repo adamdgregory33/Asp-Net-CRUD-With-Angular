ae9d5f1e5bdeff14049c6a3eff45cba164e04b24
Adam Gregory <adamdgregory33@users.noreply.github.com>
2022-05-13 20:03:42 +0100

    C3 - Full CRUD Integration

--- UI/cards/src/app/app.component.ts
+++ UI/cards/src/app/app.component.ts
@@ -10,6 +10,7 @@ import { CardsService } from './service/cards.service';
 export class AppComponent implements OnInit{
   title = 'cards';
   cards: Card[] = [];
+  card: Card = AppComponent.createCard();
 
   constructor(private cardsService: CardsService)
   {
@@ -24,5 +25,45 @@ export class AppComponent implements OnInit{
       response =>
       this.cards = response
     );
+  }
+
+  onSubmitCard(){
+    // No card id means that we are creating a new card as opposed to an existing card
+    if(this.card.id === ''){
+      this.cardsService.addCard(this.card)
+      .subscribe(
+        response => {
+          this.getAllCards();
+          this.card = AppComponent.createCard();
+        }
+      );
+    }
+    else{
+      this.cardsService.updateCard(this.card).subscribe(response =>{
+          this.getAllCards();
+      });
+    }
+  }
+
+  onDeleteCard(id: string){
+    this.cardsService.deleteCard(id)
+    .subscribe(response =>{
+      this.getAllCards();
+    });
+  }
+
+  populateForm(card: Card){
+    this.card = card;
+  }
+
+  static createCard(): Card{
+    return {
+      id:'',
+      cardNumber:'',
+      cardholderName:'',
+      expiryMonth:'',
+      expiryYear:'',
+      secureCode:''
+    }
   }
 }
--- UI/cards/src/app/service/cards.service.ts
+++ UI/cards/src/app/service/cards.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs';
 import { Card } from '../models/card.model';
+import { identifierName } from '@angular/compiler';
 
 
 @Injectable({
@@ -16,6 +17,20 @@ export class CardsService {
   // Get all cards
   getAllCards() : Observable<Card[]>{
     return this.http.get<Card[]>(this.baseUrl);
+  }
+  
+  addCard(card: Card) : Observable<Card>{
+    card.id = '00000000-0000-0000-0000-000000000000';
+
+    return this.http.post<Card>(this.baseUrl, card);
+  }
+
+  deleteCard(id:string) : Observable<Card>{
+    return this.http.delete<Card>(this.baseUrl + '/' + id);
+  }
+
+  updateCard(card: Card) : Observable<Card>{
+    return this.http.put<Card>(this.baseUrl + '/' + identifierName, card);
   }
 
 }
